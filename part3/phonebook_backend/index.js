707e4298b2c5c095a5c1c4b03d81eb63a7b46558
Sagan Krishna Gupta <sagankrishnagupta@gmail.com>
2026-06-23 00:13:26 +0530

    Completed basic RESTful API of Part3 and going to deploy notes app to internet

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json()) //JSON parser
+morgan.token('post-body', (request, response) => {
+    if(request.method === 'POST'){
+        return JSON.stringify(request.body)
+    }
+    return ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 let persons = [
   {
--- part3/theory1/index.js
+++ part3/theory1/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
-app.use(express.json()) //JSON parser
 
 let notes = [
   {
@@ -20,6 +19,18 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors())
+app.use(express.json()) //JSON parser
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -74,7 +85,13 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
