b785248f3db7cc2cccf60f545ce8422aa5bde052
Sagan Krishna Gupta <sagankrishnagupta@gmail.com>
2026-06-23 03:19:14 +0530

    uibuild

--- part3/theory1/index.js
+++ part3/theory1/index.js
@@ -28,8 +28,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.static('dist'))
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json()) //JSON parser
 app.use(requestLogger)
 
